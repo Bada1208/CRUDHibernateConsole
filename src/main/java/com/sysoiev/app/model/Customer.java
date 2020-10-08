ea9c0bd83221a80ffe57124ef6118e22329dec8d
Bada1208 <sysoievbogdan@gmail.com>
2020-10-08 13:34:54 +0200

    adjust customer

--- src/main/java/com/sysoiev/app/model/Customer.java
+++ src/main/java/com/sysoiev/app/model/Customer.java
@@ -1,34 +1,109 @@
 package com.sysoiev.app.model;
 
-import lombok.*;
 import javax.persistence.*;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
-
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name="customers")
+@Table(name = "customers")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    @Column(name="name")
+    @Column(name = "name")
     private String name;
-    @Column(name="surname")
+    @Column(name = "surname")
     private String surname;
+    @Column(name = "account_id")
+    @OneToOne(cascade = {CascadeType.PERSIST})
+    private Account customerAccount;
+    @OneToMany(mappedBy = "customer_specialties", fetch = FetchType.LAZY)
+    private Set<Specialty> customerSpecialties = new HashSet<>();
 
-    public Customer(String name, String surname, Account account, Set<Specialty> specialtySet) {
+    public Customer() {
     }
-   /* @OneToMany(mappedBy = "customers", fetch = FetchType.LAZY)
-    private Set<Specialty> customerSpecialties = new HashSet<>();*/
-    /*@Column(name="account_id")
-    private Account customerAccount;*/
+
+    public Customer(Long id) {
+        this.id = id;
+    }
+
+    public Customer(Long id, String name, String surname, Account account, Set<Specialty> specialties) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.customerAccount = account;
+        this.customerSpecialties = specialties;
+    }
+
+    public Customer(Long id, String name, String surname, Account account) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.customerAccount = account;
+    }
+
+    public Customer(Long id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCustomerSpecialties(Specialty customerSpecialty) {
+        this.customerSpecialties.add(customerSpecialty);
+    }
+
+    public void setCustomerSpecialtiesSet(Set<Specialty> customerSpecialtiesSet) {
+        this.customerSpecialties = customerSpecialtiesSet;
+    }
+
+    public void setCustomerAccount(Account customerAccount) {
+        this.customerAccount = customerAccount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Set<Specialty> getCustomerSpecialtiesSet() {
+        return customerSpecialties;
+    }
+
+    public Account getCustomerAccount() {
+        return customerAccount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialties() {
+        String specialtyString = "";
+        for (Specialty s : customerSpecialties) {
+            specialtyString += "{" + s.getId() + "}";
+        }
+        return specialtyString;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + name + " " + surname + " " + customerAccount.getId() + " " + getSpecialties();
+    }
 
 }
